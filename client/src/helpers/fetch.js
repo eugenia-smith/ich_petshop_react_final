8ab8f963971e8127ab19815e57e1beab16ff09ac
Eugenia Smith <ev-genius4@mail.ru>
2024-10-14 10:22:42 +0200

    query logics moved to a separate file

---
+++ client/src/helpers/fetch.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+export const fetchCategories = async (setter) => {
+  try {
+    const { data: response } = await axios.get(
+      "http://localhost:3333/categories/all"
+    );
+    setter(response);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+export const fetchCategory = async (setter, id) => {
+  try {
+    const { data: response } = await axios.get(
+      `http://localhost:3333/categories/${id}`
+    );
+    setter(response);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+export const fetchProducts = async (setter) => {
+  try {
+    const { data: response } = await axios.get(
+      "http://localhost:3333/products/all"
+    );
+    setter(response);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+export const fetchSales = async (setter) => {
+  try {
+    const { data: response } = await axios.get(
+      "http://localhost:3333/products/all"
+    );
+    setter(response.filter((elem) => elem.discont_price !== null));
+  } catch (error) {
+    console.error(error.message);
+  }
+};
