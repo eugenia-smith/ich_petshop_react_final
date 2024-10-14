1dabe3ac7a096234dfcb9d8752851249cd38dc57
Eugenia Smith <ev-genius4@mail.ru>
2024-10-14 11:34:16 +0200

    styling of the sales section is completed

--- client/src/pages/sales/Sales.jsx
+++ client/src/pages/sales/Sales.jsx
@@ -1,7 +1,7 @@
 import styles from "./styles.module.css";
 import { useState, useEffect } from "react";
-
 import SaleCard from "../../components/saleCard";
+import { Link } from "react-router-dom";
 
 import { fetchSales } from "../../helpers/fetch";
 
@@ -12,20 +12,25 @@ function Sales() {
   }, []);
 
   return (
-    <main className={styles.main_container}>
-      <section className={styles.sales_section}>
-        <div className={styles.main_categories_header}>
-          <h2 className={styles.section_heading}>All sales</h2>
-        </div>
-        <ul className={styles.categories_list}>
-          {sales.map((elem, index) => (
-            <li key={index}>
-              <SaleCard elem={elem} />
-            </li>
-          ))}
-        </ul>
-      </section>
-    </main>
+    <section className={styles.sales_section}>
+      <div className={styles.breadcrumbs}>
+        <Link to={"/"}>
+          <div>Main page</div>
+        </Link>
+        <div>All sales</div>
+      </div>
+
+      <div className={styles.main_categories_header}>
+        <h2 className={styles.section_heading}>Discounted items</h2>
+      </div>
+      <ul className={styles.categories_list}>
+        {sales.map((elem, index) => (
+          <li key={index}>
+            <SaleCard elem={elem} />
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
 
