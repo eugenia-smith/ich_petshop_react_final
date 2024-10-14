31feb3e5d1e755b934cec5cd397b8c48d4afe09c
Eugenia Smith <ev-genius4@mail.ru>
2024-10-14 15:24:43 +0200

    filters block installed

--- client/src/pages/products/Products.jsx
+++ client/src/pages/products/Products.jsx
@@ -1,11 +1,39 @@
 import { Link } from "react-router-dom";
-
 import styles from "./styles.module.css";
 import { useState, useEffect } from "react";
-
 import SaleCard from "../../components/saleCard";
+import React from "react";
+import { InputNumber, Checkbox, Button, Dropdown, Space, message } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 
 import { fetchProducts } from "../../helpers/fetch";
+
+const handleMenuClick = (e) => {
+  message.info("Click on menu item.");
+  console.log("click", e);
+};
+const items = [
+  {
+    label: "by default",
+    key: "1",
+  },
+  {
+    label: "newest",
+    key: "2",
+  },
+  {
+    label: "price: high-low",
+    key: "3",
+  },
+  {
+    label: "price: low-high",
+    key: "4",
+  },
+];
+const menuProps = {
+  items,
+  onClick: handleMenuClick,
+};
 
 function Products() {
   const [products, setProducts] = useState([]);
@@ -24,6 +52,29 @@ function Products() {
 
       <div className={styles.main_categories_header}>
         <h2 className={styles.section_heading}>All products</h2>
+        <div className={styles.filters_container}>
+          <label className={styles.price_range_container}>
+            Price
+            <InputNumber min={1} max={1000} defaultValue={"from"} />
+            <InputNumber min={1} max={1000} defaultValue={"to"} />
+          </label>
+
+          <label>
+            Discounted items
+            <Checkbox />
+          </label>
+          <label>
+            Sorted
+            <Dropdown menu={menuProps}>
+              <Button>
+                <Space>
+                  by default
+                  <DownOutlined />
+                </Space>
+              </Button>
+            </Dropdown>
+          </label>
+        </div>
       </div>
       <ul className={styles.categories_list}>
         {products.map((elem, index) => (
--- client/src/pages/sales/Sales.jsx
+++ client/src/pages/sales/Sales.jsx
@@ -2,8 +2,37 @@ import styles from "./styles.module.css";
 import { useState, useEffect } from "react";
 import SaleCard from "../../components/saleCard";
 import { Link } from "react-router-dom";
+import { InputNumber, Button, Dropdown, Space, message } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 
 import { fetchSales } from "../../helpers/fetch";
+
+const handleMenuClick = (e) => {
+  message.info("Click on menu item.");
+  console.log("click", e);
+};
+const items = [
+  {
+    label: "by default",
+    key: "1",
+  },
+  {
+    label: "newest",
+    key: "2",
+  },
+  {
+    label: "price: high-low",
+    key: "3",
+  },
+  {
+    label: "price: low-high",
+    key: "4",
+  },
+];
+const menuProps = {
+  items,
+  onClick: handleMenuClick,
+};
 
 function Sales() {
   const [sales, setSales] = useState([]);
@@ -22,6 +51,25 @@ function Sales() {
 
       <div className={styles.main_categories_header}>
         <h2 className={styles.section_heading}>Discounted items</h2>
+        <div className={styles.filters_container}>
+          <label className={styles.price_range_container}>
+            Price
+            <InputNumber min={1} max={1000} defaultValue={"from"} />
+            <InputNumber min={1} max={1000} defaultValue={"to"} />
+          </label>
+
+          <label>
+            Sorted
+            <Dropdown menu={menuProps}>
+              <Button>
+                <Space>
+                  by default
+                  <DownOutlined />
+                </Space>
+              </Button>
+            </Dropdown>
+          </label>
+        </div>
       </div>
       <ul className={styles.categories_list}>
         {sales.map((elem, index) => (
